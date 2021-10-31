eef287a5b805f532fc13017c7cf2adaa0e9c8d6d
kwynncom <buessresume@gmail.com>
2021-10-31 00:02:24 -0400

    fix attempt config path

--- back/config.php
+++ back/config.php
@@ -2,6 +2,6 @@
 
 function getChronyParserPath() {
 	if (!isAWS()) return __DIR__ .				   '/../../nss/chronyParsed.php';
-	else		  return __DIR__ . '/../../../20/10/timeserver/chronyParsed.php';
+	else		  return __DIR__ . '/../../../../20/10/timeserver/chronyParsed.php';
 }
 
