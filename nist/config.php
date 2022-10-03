6310c54e482842df3e9036f4789ae970e77f4a55
kwynncom <buessresume@gmail.com>
2022-10-03 04:51:26 -0400

    tweaks

--- nist/config.php
+++ nist/config.php
@@ -6,5 +6,6 @@ interface callSNTPConfig {
 	const collname = 'calls';
 	const NISTminS  = 4;
 	const cronIntervalM = 4; // does not have to stay in sync with cron, but probably best
+	const cronIntervalS = self::cronIntervalM * 60;
 }
 
--- nist/cronDo.php
+++ nist/cronDo.php
@@ -9,11 +9,14 @@ if (iscli()) {
 	if (1) {
 		rd('wait', $ws);
 
-		$maxws = callSNTPConfig::cronIntervalM * 60 - callSNTPConfig::NISTminS - 1;
+		$maxws = callSNTPConfig::cronIntervalS;
 
 		if ($ws > $maxws) exit(0);
+		
+		$wmin = max($ws, 0);
 
-		$wfus  = random_int(roint($ws * M_MILLION), $maxws * M_MILLION);
+		$wfus  = random_int(roint($wmin * M_MILLION), $maxws * M_MILLION);
+		rd('min / max (S) = ', $wmin, $maxws);
 		rd('sleeping for x s = ', $wfus / M_MILLION);
 		if ($wfus > 0) usleep($wfus);
 	}
@@ -22,7 +25,7 @@ if (iscli()) {
 	rd(print_r($dat, true));
 }
 
-function rd($a = '', $b = '') {
+function rd($a = '', $b = '', $c = '') {
 	$r = date('r');
-	file_put_contents('/tmp/kwsncronr.txt', "$r : $a $b \n", FILE_APPEND);	
+	file_put_contents('/tmp/kwsncronr.txt', "$r : $a $b $c \n", FILE_APPEND);	
 }
