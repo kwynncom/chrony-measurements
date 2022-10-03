3c88d196fb934e7d83d8b40a1603534fbad70d15
kwynncom <buessresume@gmail.com>
2022-10-02 22:52:28 -0400

    added details to pass-fail

--- nist/passfail.php
+++ nist/passfail.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once('/opt/kwynn/kwutils.php');
-require_once('config.php');
+require_once(__DIR__ . '/config.php');
 
 
 class nist_passfail extends dao_generic_3 implements callSNTPConfig {
@@ -10,18 +10,30 @@ class nist_passfail extends dao_generic_3 implements callSNTPConfig {
 	const withinS = self::withinM * 60;
 	const mustn   = 3;
 	const tolerancens = M_MILLION;
-	const tolerancensTest = self::tolerancens * 50;
-	
+	const tolerancensTest = self::tolerancens * 60;
 	
 	private function __construct() {
 		parent::__construct(self::dbname);
 		$this->creTabs(self::collname);
-		$this->do10();
+		$this->init();
+		try { $this->do10(); } catch (Exception $ex) { return; }
+		$this->ores->pass = true;
+	}
+	
+	public function getRes() { return $this->ores; }
+	
+	private function init() {
+		$this->ores = new stdClass();
+		$this->ores->pass = false;
+		$this->ores->otrcnt = 0;
+		$this->ores->offs = [];
+		$this->ores->iskwdev = ispkwd();
 	}
 	
 	private function do10() {
 		$res = $this->ccoll->find(['U' => ['$gte' => time() - self::withinS]], ['sort' => ['U' => -1], 'limit' => 5]);
 		kwas($res && is_array($res) && count($res) >= self::mustn, 'SNTP pf count fail 1 - 2121');
+		$this->ores->otrcnt = count($res);
 		$this->do20($res);
 		return;
 	}
@@ -34,6 +46,7 @@ class nist_passfail extends dao_generic_3 implements callSNTPConfig {
 			$a = $r['t4Uns'];
 			try { 
 				$o = self::SNTPOffset($a); kwas($o && is_numeric($o), 'bad form of passfail NIST SNTP result');
+				$this->ores->offs[] = $o;
 				kwas(self::passfail($o), 'not within tolerance SNTP passfail');
 				$ti++;
 			} catch (Exception $ex) {}
@@ -60,6 +73,11 @@ class nist_passfail extends dao_generic_3 implements callSNTPConfig {
 	
 	public static function SNTPOffset($T) {
 		return ((($T[1] - $T[0]) + ($T[2] - $T[3]))) >> 1;
+	}
+	
+	public static function getDets() {
+		$o = new self();
+		return $o->getRes();
 	}
 	
 	public static function ispass() {
