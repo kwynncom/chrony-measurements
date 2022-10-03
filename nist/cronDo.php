7ce3d4e46bdf62620d72d2a544bc5ea22c23c68b
kwynncom <buessresume@gmail.com>
2022-10-03 04:18:50 -0400

    ready to go, I think

--- nist/cronDo.php
+++ nist/cronDo.php
@@ -2,18 +2,25 @@
 
 require_once(__DIR__ . '/callSNTPReg.php');
 
-$ws  = nist_backoff_calls::getWait();
-rd('wait', $ws);
+if (iscli()) {
 
-$maxws = callSNTPConfig::cronIntervalM * 60 - callSNTPConfig::NISTminS - 1;
+	$ws  = nist_backoff_calls::getWait();
 
-if ($ws > $maxws) exit(0);
+	if (1) {
+		rd('wait', $ws);
 
-$wfus  = random_int(roint($ws * M_MILLION), $maxws * M_MILLION);
-rd('sleeping for x s = ', $wfus / M_MILLION);
-usleep($wfus);
+		$maxws = callSNTPConfig::cronIntervalM * 60 - callSNTPConfig::NISTminS - 1;
 
-nist_backoff_calls::get();
+		if ($ws > $maxws) exit(0);
+
+		$wfus  = random_int(roint($ws * M_MILLION), $maxws * M_MILLION);
+		rd('sleeping for x s = ', $wfus / M_MILLION);
+		if ($wfus > 0) usleep($wfus);
+	}
+
+	$dat = nist_backoff_calls::get();
+	rd(print_r($dat, true));
+}
 
 function rd($a = '', $b = '') {
 	$r = date('r');
--- nist/crontest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$ppid = posix_getppid();
-file_put_contents('/tmp/ppid', $ppid . "\n");
-echo($ppid . "\n");
-sleep(120);
-
-// $ pstree -s ppid
