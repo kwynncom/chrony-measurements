a82f6cc7fba8795353af10b727f4eb006aa75b51
kwynncom <buessresume@gmail.com>
2022-10-09 20:05:25 -0400

    attempt to daemonize not working

--- logscl.php
+++ logscl.php
@@ -6,6 +6,7 @@ class chronylog_cli_filter {
 
 	const linesn = 100;
 	const thef   = '/var/log/chrony/measurements.log';
+	const dlockf = '/var/kwynn/mysd/loglock';
 
 	private function procIP($hu, $ip, $restl) {
 		static $o = false;
@@ -53,7 +54,8 @@ class chronylog_cli_filter {
 		$this->oout($ones[16]);
 		$restl = substr($ones, 19, 66);
 		$this->oout($restl);
-		if ($this->isBatch) $this->procIP($hu, $ip, $restl);
+		// if ($this->isBatch) 
+		$this->procIP($hu, $ip, $restl);
 		$this->oout("\n");
 		$this->oi++;
 		return;
@@ -64,13 +66,56 @@ class chronylog_cli_filter {
 	private function outHeader() {
 		static $o = false;
 		$s = <<<CHRH
-   Date (UTC) Time   IP 6 LP RP Score    Offset  Peer del.  Peer disp. Root del.  Root disp. Refid     MTxRx
+============================================================================================
+   Date (UTC) Time   IP 6 LP RP Score    Offset  Peer del.  Peer disp. Root del.  Root disp.
+============================================================================================
 CHRH;
-		if ($o === false) $o = substr(rtrim($s), 0, 91);
+		if ($o === false) $o = rtrim($s); // substr(rtrim($s), 0, 91);
 		$this->oout($o . "\n");
 	}
 	
+	private function amIDaemon() {
+		global $argc, $argv;
+		
+		for($i=1; $i < $argc; $i++) if ($argv[$i] === '-d') return true;
+		return false;
+	}
+	
+	private function __destruct() {
+		$this->cleanupf();
+		
+	}
+	
+	private function cleanupf() {
+		if (!$this->h) return;
+		if ($this->hasLock) flock($this->h, LOCK_UN);
+		$this->hasLock = false;
+		fclose($this->h);
+		$this->h = false;
+	}
+	
+	private function daemonize() {
+		$this->h = false;
+		$this->hasLock = false;
+		$this->h = $h = fopen(self::dlockf, 'r');
+		
+		if ($amd = $this->amIDaemon()) $lockty = LOCK_EX;
+		else $lockty = LOCK_SH;
+		
+		$this->hasLock = $l = flock($h, $lockty | LOCK_NB);
+		if ($l && !$amd) {
+			$this->cleanupf();
+			kwnohup('php ' . __FILE__ . ' -d');
+			if ($this->isBatch) exit(0);
+		}
+		
+		// if (!$amd) $this->cleanupf();
+	}
+	
 	private function init() {
+		
+		$this->daemonize();
+
 		$this->oi = 0;
 		
 		if (!$this->isBatch) {
