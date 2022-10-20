698d2b64a55bf2a0bb44e1e3d8e3490359689644
kwynncom <buessresume@gmail.com>
2022-10-20 00:18:27 -0400

    working on highest poll value output

--- nist/queries.js
+++ nist/queries.js
@@ -1,3 +1,18 @@
+db.getCollection('calls').aggregate(
+[
+{$match : {'U' : {'$gte' : new Date().getTime() / 1000 - 86400}}}
+]
+)
+
 printjson(db.getCollection('calls').find({}, { _id : 0, 'r' : 1, 'via' : 1}).toArray())
 
 printjson(db.getCollection('calls').find({}, { _id : 0, 'r' : 1, 'via' : 1}).sort({'U' : -1}).toArray())
+
+db.getCollection('calls').aggregate(
+[
+{$match : {'U' : {'$gte' : new Date().getTime() / 1000 - 86400}, offset : {$exists : true}}},
+{$project : {absoff : {$abs : '$offset'}}},
+{$sort : {absoff : -1}},
+{$limit : 20}
+]
+)
---
+++ nist/summary.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once(__DIR__ . '/config.php');
+
+class nist_summary extends dao_generic_3 {
+	
+	public static function get() {
+		new self();
+	}
+	
+	private function __construct() {
+		parent::__construct(callSNTPConfig::dbname);
+		$this->creTabs(['c' => callSNTPConfig::collname, 's' => 'start']);
+		$this->do05();
+		$this->do10();
+	}
+	
+	private function do05() {
+		static $key = 'active (running) since ';
+		static $keyl = false;
+		if ($keyl === false) $keyl = strlen($key);
+				
+		
+		$t = shell_exec('systemctl status chrony | head -n 3');
+		if (($sp = strpos($t, $key)) === false) return;
+		//						  123456789012345678901234567
+		// active (running) since Wed 2022-10-19 06:04:13 EDT
+		$s = substr($t, $sp + $keyl, 27);
+		$U = strtotimeRecent($s);
+		$q = ['U' => $U];
+		if ($this->scoll->findOne($q)) return;
+		$dat = $q;
+		$dat['r'] = date('r', $U);
+		$dat['_id'] = dao_generic_3::get_oids(false, $U);
+		$this->scoll->insertOne($dat, ['kwnoup' => true]);
+		return;
+	}
+	
+	private function do10() {
+		$q = ['U' => ['$gte' => time() - DAY_S * 2], 'Uns4' => ['$exists' => true]];
+		$q['offset'] = ['$exists' => false];
+		$a = $this->ccoll->find($q);
+		foreach($a as $r) {
+			$off = sntpSanity::SNTPOffset($r['Uns4']) / M_BILLION;
+			$this->ccoll->upsert(['_id' => $r['_id']], ['offset' => $off]);
+		}
+		return;
+	}
+	
+}
+
+if (didCLICallMe(__FILE__)) nist_summary::get();
