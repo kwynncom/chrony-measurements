cb3b2eb0a897b14ea542c3efbdd755213b4781aa
kwynncom <buessresume@gmail.com>
2022-10-22 05:17:06 -0400

    yet more tweaks

--- nist/node/sntpWorst.js
+++ nist/node/sntpWorst.js
@@ -1,14 +1,12 @@
 class sntpWorst {
 	
-	constructor() {	this.omclip = require('mongodb').MongoClient.connect('mongodb://localhost/');	}	
-
+	constructor()	   {					this.omclip = require('mongodb').MongoClient.connect('mongodb://localhost/');	}	
 	async defineColl() { this.coll = (await this.omclip).db('sntp4').collection('calls');	}
-
 	async get() {
 		if (!this.coll) await this.defineColl();
         return await this.coll.aggregate(
             [
-                {$match : {'U' : {'$gte' : ((new Date().getTime()) / 1000) - 172800 }, offset : {$exists : true}}},
+                {$match : {'U' : {'$gte' : new Date().getTime() / 1000 - 172800 }, offset : {$exists : true}}},
                 {$project : {absoff : {$abs : '$offset'}, _id : 0, U : 1, r : 1}},
                 {$sort : {absoff : -1}},
                 {$limit : 20}
@@ -17,13 +15,9 @@ class sntpWorst {
     }
 	
 	static async fromcli() {
-		const o = new sntpWorst();
-		const r = await o.get();
-		console.log(r);	
+		console.log(await new sntpWorst().get());
 		process.exit();
 	}	
 }
-
 if (require.main === module) sntpWorst.fromcli();
-
 module.exports = sntpWorst;
