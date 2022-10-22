42159db590cafb40a76419f59388e82cabad2173
kwynncom <buessresume@gmail.com>
2022-10-22 05:12:28 -0400

    yet more tweaks

--- nist/node/sntpWorst.js
+++ nist/node/sntpWorst.js
@@ -1,40 +1,29 @@
-module.exports = class sntpWorst {
+class sntpWorst {
 	
-	config() { 	this.daysBack = 2;	}
-	
-	constructor() {
-		this.config();
-		this.SBack = this.daysBack * 86400;
-		const MongoClient = require('mongodb').MongoClient;
-		this.omclip = MongoClient.connect('mongodb://localhost/');
-	}	
+	constructor() {	this.omclip = require('mongodb').MongoClient.connect('mongodb://localhost/');	}	
 
-	async defineColl() {
-		const cli = await this.omclip;
-		this.coll = cli.db('sntp4').collection('calls');
-	}
+	async defineColl() { this.coll = (await this.omclip).db('sntp4').collection('calls');	}
 
 	async get() {
-
 		if (!this.coll) await this.defineColl();
-
         return await this.coll.aggregate(
             [
-                {$match : {'U' : {'$gte' : ((new Date().getTime()) / 1000) - this.SBack }, offset : {$exists : true}}},
+                {$match : {'U' : {'$gte' : ((new Date().getTime()) / 1000) - 172800 }, offset : {$exists : true}}},
                 {$project : {absoff : {$abs : '$offset'}, _id : 0, U : 1, r : 1}},
                 {$sort : {absoff : -1}},
                 {$limit : 20}
             ]
         ).toArray();  
     }
+	
+	static async fromcli() {
+		const o = new sntpWorst();
+		const r = await o.get();
+		console.log(r);	
+		process.exit();
+	}	
 }
 
-if (require.main === module) { 
-	(async() => {
-		const ref = module.exports;
-		const o = new ref();
-		const r = await Promise.resolve(o.get());
-		console.log(r);
-		process.exit();
-	})();
-}
+if (require.main === module) sntpWorst.fromcli();
+
+module.exports = sntpWorst;
